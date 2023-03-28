d420c283aecad448fd41c4fbde049748caf264f7
nurzuriana <nurzurianahabil@gmail.com>
2023-03-29 07:00:25 +0800

    add new

---
+++ MyApplication/logout/default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace mykraf
+{
+    public partial class LogoutMykraf : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session["uname"] = null;
+            Session["pwd"] = null;
+            Session.Abandon();
+            string backURL = Server.HtmlEncode(HttpContext.Current.Request.ServerVariables["HTTP_REFERER"]);
+            Response.Redirect(backURL);
+        }
+    }
+}
